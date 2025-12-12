33cde611ec97c9eebbf5e4a1c5f990dbedbce6ed
Bharath-190 <bharathpadamukhi@gmail.com>
2025-12-13 04:27:40 +0530

    feat:canvas studio

--- apps/frontend/apps/frontend/lib/firebase/init.ts
+++
@@ -1,9 +0,0 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp, getApps } from "firebase/app";
-import { getAuth } from 'firebase/auth';
-import firebaseConfig from './config';
-
-// Initialize Firebase only if it hasn't been initialized already
-const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
-export const auth = getAuth(app);
-export default app;
